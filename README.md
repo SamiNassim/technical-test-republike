ff54cba4069e9b9c94a6ebc24a0edae5b71b8ebd
Sami Nassim <sami.bououdine@gmail.com>
2024-04-07 01:29:44 +0200

    chore: Readme update

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # Republike
 
-<i>Instructions coming soon...</i>
+Voici le résultat de mon test technique pour Republike. </br>
+J'ai délibérément push les fichiers .env pour faciliter le lancement de l'application.
+
+## Instructions
+
+Vous aurez besoin de **Docker** pour faire fonctionner la base de donnée PostgreSQL.
+
+Une fois que Docker est installé et après avoir redémarrer votre PC, lancez Docker.
+
+Ensuite :
+
+```` 
+git clone https://github.com/SamiNassim/republike.git
+````
+
+```` 
+cd republike
+````
+
+```` 
+npm install
+````
+```` 
+docker compose up -d
+````
+
+```` 
+npx prisma db push
+````
+
+```` 
+npm run dev
+````
+
+## Temps passé sur le test
+
+J'ai passé approximativement entre 20~30h sur ce test. </br>
+J'ai pris la liberté de modifier le texte "Create profile" sur la page de Login qui ne correspondait pas sur la maquette.
--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
     return (
         <nav className="fixed w-full top-0 h-16 border-b-1 border-[#E8E9EB] bg-white z-30">
             <div className="flex flex-row justify-center items-center w-full h-full">
                 <div className="absolute left-10">
-                    <Image
-                        src="/logoblack.svg"
-                        width={36}
-                        height={36}
-                        alt="Logo Republik"
-                        color="black"
-                    />
+                    <Link href="/home">
+                        <Image
+                            src="/logoblack.svg"
+                            width={36}
+                            height={36}
+                            alt="Logo Republik"
+                            color="black"
+                        />
+                    </Link>
                 </div>
                 <div>
                     <p className="text-xl font-bold">Home</p>
